913101b9151463abb2381851a9fb452f1b8782e9
beneenfune <103278030+beneenfune@users.noreply.github.com>
2022-05-30 22:50:54 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,15 +1,17 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-import java.util.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+
 public class MarkdownParseTest {
+
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
     @Test
     public void testGetLinksFile1() throws IOException{
         Path fileName = Path.of("test-file.md");
